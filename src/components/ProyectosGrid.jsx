3ccc220ed2556e06f3ea2fa15f0a4841509aabc9
FLRSscript <flrsscript@users.noreply.github.com>
2024-03-16 10:49:16 -0600

    asda

--- src/components/ProyectosGrid.jsx
+++ src/components/ProyectosGrid.jsx
@@ -10,23 +10,23 @@ export default function ProyectosGrid() {
   return (
     <div className="grid grid-cols-3 grid-rows-34 gap-4 p-4">
       <div className="col-span-2 row-span-12 rounded-xl box p-2 border-4 border-black relative">
-        <div className="bg-cover bg-center absolute inset-0 rounded-xl" style={{ backgroundImage: "url('src/assets/graph.png')", ...imagenStyle }}></div>
+        <div className="bg-cover bg-center absolute inset-0 rounded-xl" style={{ backgroundImage: "url('public/assets/graph.png')", ...imagenStyle }}></div>
         <Typography className="relative z-10">Nombre de un Proyecto</Typography>
       </div>
       <div className="col-span-1 row-span-8 rounded-xl box p-2 border-4 border-black relative">
-        <div className="bg-cover bg-center absolute inset-0 rounded-xl" style={{ backgroundImage: "url('/src/assets/bmo.png')", ...imagenStyle }}></div>
+        <div className="bg-cover bg-center absolute inset-0 rounded-xl" style={{ backgroundImage: "url('/public/assets/bmo.png')", ...imagenStyle }}></div>
         <Typography className="relative z-10">Nombre de un Proyecto</Typography>
       </div>
       <div className="col-span-1 row-span-12 rounded-xl box p-2 border-4 border-black relative">
-        <div className="bg-cover bg-center absolute inset-0 rounded-xl" style={{ backgroundImage: "url('/src/assets/foto.png')", ...imagenStyle }}></div>
+        <div className="bg-cover bg-center absolute inset-0 rounded-xl" style={{ backgroundImage: "url('/public/assets/foto.png')", ...imagenStyle }}></div>
         <Typography className="relative z-10">Nombre de un Proyecto</Typography>
       </div>
       <div className="col-span-1 row-span-8 rounded-xl box p-2 border-4 border-black relative">
-        <div className="bg-cover bg-center absolute inset-0 rounded-xl" style={{ backgroundImage: "url('/src/assets/cuadricula.jpg')", ...imagenStyle }}></div>
+        <div className="bg-cover bg-center absolute inset-0 rounded-xl" style={{ backgroundImage: "url('/public/assets/cuadricula.jpg')", ...imagenStyle }}></div>
         <Typography className="relative z-10">Nombre de un Proyecto</Typography>
       </div>
       <div className="col-span-1 row-span-8 rounded-xl box p-2 border-4 border-black relative">
-        <div className="bg-cover bg-center absolute inset-0 rounded-xl" style={{ backgroundImage: "url('src/assets/macintosh.png')", ...imagenStyle }}></div>
+        <div className="bg-cover bg-center absolute inset-0 rounded-xl" style={{ backgroundImage: "url('public/assets/macintosh.png')", ...imagenStyle }}></div>
         <Typography className="relative z-10">Nombre de un Proyecto</Typography>
       </div>
     </div>
