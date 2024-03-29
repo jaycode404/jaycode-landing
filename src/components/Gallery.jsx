f3ccb4ebcf0cae784c1eac134c4a32ad6bfc5e8b
FLRSscript <flrsscript@users.noreply.github.com>
2024-03-29 14:46:02 -0600

    gallery done

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,52 +1,42 @@
 export function Gallery() {
+  const data = [
+    {
+      imageLink:
+        "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      colSpan: 2, // Indica que esta imagen ocupará 2 columnas
+      rowSpan: 1, // Indica que esta imagen ocupará 1 fila
+    },
+    {
+      imageLink:
+        "https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+      colSpan: 1, // Indica que esta imagen ocupará 1 columna
+      rowSpan: 2, // Indica que esta imagen ocupará 2 filas
+    },
+    {
+      imageLink:
+        "https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80",
+      colSpan: 1, // Indica que esta imagen ocupará 1 columna
+      rowSpan: 1, // Indica que esta imagen ocupará 1 fila
+    },
+    {
+      imageLink:
+        "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
+      colSpan: 1, // Indica que esta imagen ocupará 1 columna
+      rowSpan: 1, // Indica que esta imagen ocupará 1 fila
+    },
+  ];
+
   return (
-    <div className="md:max-w-[80%] h-[auto] grid grid-cols-2 lg:grid-cols-3 gap-2 px-2 m-auto ">
-      <div className="grid ">
-        <div>
+    <div className="grid grid-cols-2 gap-2">
+      {data.map(({ imageLink, colSpan, rowSpan }, index) => (
+        <div key={index} style={{ gridColumn: `span ${colSpan}`, gridRow: `span ${rowSpan}` }}>
           <img
-            className="imagen-gallery"
-            src="https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-            alt="gallery-photo"
+            className="h-full w-full rounded-lg object-cover object-center"
+            src={imageLink}
+            alt=""
           />
         </div>
-        <div>
-          <img
-            className="imagen-gallery"
-            src="https://images.unsplash.com/photo-1629367494173-c78a56567877?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=927&q=80"
-            alt="gallery-photo"
-          />
-        </div>
-        <div>
-          <img
-            className="imagen-gallery"
-            src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-            alt="gallery-photo"
-          />
-        </div>
-      </div>
-      <div className="grid gap-4">
-        <div>
-          <img
-            className="imagen-gallery"
-            src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-            alt="gallery-photo"
-          />
-        </div>
-        <div>
-          <img
-            className="imagen-gallery"
-            src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-            alt="gallery-photo"
-          />
-        </div>
-        <div>
-          <img
-            className="imagen-gallery"
-            src="https://docs.material-tailwind.com/img/team-3.jpg"
-            alt="gallery-photo"
-          />
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
