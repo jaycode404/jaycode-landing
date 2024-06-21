9c4e0cc5f9052f526754b6fd2d4579bbc1006cfe
FLRSscript <flrsscript@users.noreply.github.com>
2024-06-21 14:53:35 -0700

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,11 +36,29 @@ export default function Header() {
             jacob<span className="dot">.</span>
           </h1>
 
-          <p className="side-border">Me gusta crear aplicaciones útiles para mi entorno
-
-           es divertidoasdasdddddddddddddddddddd 
+          <p className="side-border">
+            Me gusta crear aplicaciones útiles para mi entorno es
+            divertidoasdasdddddddddddddddddddd
           </p>
-          <button className="button">hola</button>
+
+          <button className="button">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-user icon"
+            >
+              <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle>
+            </svg>
+            CV
+          </button>
         </div>
 
         <div className="hero-img"></div>
