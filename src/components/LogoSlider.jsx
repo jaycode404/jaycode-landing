b517abb1945303e6203536434ca3b9edc39d96ca
FLRSscript <flrsscript@users.noreply.github.com>
2024-06-28 19:57:46 -0600

    mac slider

--- src/components/LogoSlider.jsx
+++ src/components/LogoSlider.jsx
@@ -11,24 +11,35 @@ export default function LogoSlider() {
       {/* maccintosh */}
       <img src="/assets/macintosh.png" alt="" className="mac mac-shadow" />
       {/* caroussel */}
+      <div className="div-lights"></div>
       <div className="logos">
-        <div className="logo-slider" style={sliderStyle}>
-          <img src="/assets/react-3.svg" alt="" />
-          <img src="/assets/js-logo.png" alt="" />
-          <img src="/assets/tail-logo.png" alt="" />
-          <img src="/assets/html-logo.png" alt="" />
-          <img src="/assets/css-logo.png" alt="" />
-          <img src="/assets/mysql-logo.png" alt="" className="social-invert" />
-          <img src="/assets/nodejs-logo.png" alt="" />
-        </div>
-        <div className="logo-slider" style={sliderStyle}>
-          <img src="/assets/react-3.svg" alt="" />
-          <img src="/assets/js-logo.png" alt="" />
-          <img src="/assets/tail-logo.png" alt="" />
-          <img src="/assets/html-logo.png" alt="" />
-          <img src="/assets/css-logo.png" alt="" />
-          <img src="/assets/mysql-logo.png" alt="" className="social-invert" />
-          <img src="/assets/nodejs-logo.png" alt="" />
+        <div className="screen-lights">
+          <div className="logo-slider" style={sliderStyle}>
+            <img src="/assets/react-3.svg" alt="" />
+            <img src="/assets/js-logo.png" alt="" />
+            <img src="/assets/tail-logo.png" alt="" />
+            <img src="/assets/html-logo.png" alt="" />
+            <img src="/assets/css-logo.png" alt="" />
+            <img
+              src="/assets/mysql-logo.png"
+              alt=""
+              className="social-invert"
+            />
+            <img src="/assets/nodejs-logo.png" alt="" />
+          </div>
+          <div className="logo-slider" style={sliderStyle}>
+            <img src="/assets/react-3.svg" alt="" />
+            <img src="/assets/js-logo.png" alt="" />
+            <img src="/assets/tail-logo.png" alt="" />
+            <img src="/assets/html-logo.png" alt="" />
+            <img src="/assets/css-logo.png" alt="" />
+            <img
+              src="/assets/mysql-logo.png"
+              alt=""
+              className="social-invert"
+            />
+            <img src="/assets/nodejs-logo.png" alt="" />
+          </div>
         </div>
       </div>
     </div>
