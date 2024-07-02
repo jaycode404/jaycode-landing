898361b51bc979ccacc51a96f1ab07b7facde799
FLRSscript <flrsscript@users.noreply.github.com>
2024-07-01 22:10:24 -0600

    slider pics

--- src/proyectSs.js
+++ src/proyectSs.js
@@ -1,24 +1,31 @@
 import puppeteer from "puppeteer";
 import fs from "fs";
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function takeScreenshot(url, outputPath) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  // Espera hasta que el evento "load" del DOM se complete
   await page.goto(url, { waitUntil: "load" });
+
+  // Espera de 2 segundos
+  await sleep(2000);
 
   await page.screenshot({ path: outputPath });
   await browser.close();
 }
 
 const urls = [
-  "https://jayflix-six.vercel.app/",
-  "https://jaycode-landing.vercel.app/",
+  "https://codeflix-tan.vercel.app/",
   "https://randomusic.vercel.app/",
   "https://recordatorios-crud-app.vercel.app/",
   "https://pedagogia-landing.vercel.app/",
   "https://busqueda-indexada.vercel.app/",
+  "https://burger-landing-ten.vercel.app/",
+  "https://apaloosa.vercel.app/",
 ];
 
 urls.forEach(async (url, index) => {
